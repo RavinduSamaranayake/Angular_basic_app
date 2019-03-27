551744dc3ac841fd299c1d6b43ba37fd90c44591
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-03-27 22:14:07 +0530

    working with directives

--- src/app/directives/setmycolor.directive.ts
+++ src/app/directives/setmycolor.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 import { Element } from '@angular/compiler/src/render3/r3_ast';
 
 @Directive({
@@ -6,8 +6,24 @@ import { Element } from '@angular/compiler/src/render3/r3_ast';
 })
 export class SetmycolorDirective {
 
+  @Input('fcolor') myfcolor:String;  //passing the first color using inputs
+  @Input('lcolor') mylcolor:String;  //passing the second color using inputs
+
+
   constructor(private element:ElementRef) { 
     element.nativeElement.style.color = 'yellow';
+  }
+
+  @HostListener('mouseenter') setColoronMouseEnter(){
+    this.setColor(this.myfcolor);
+  }
+
+  @HostListener('mouseleave') setColoronMouseLeave(){
+    this.setColor(this.mylcolor);
+  }
+
+  private setColor(color:String){
+    this.element.nativeElement.style.color = color;
   }
 
 }
