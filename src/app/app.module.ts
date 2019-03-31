292c6a0a6ed66505623aad0f5ed66675cfb197d1
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-03-31 14:43:31 +0530

    setting routers with nav link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SetmycolorDirective } from './directives/setmycolor.directive';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'activities', component: ActivitiesComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    SetmycolorDirective
+    SetmycolorDirective,
+    DashboardComponent,
+    ActivitiesComponent,
+   
   ],
+  
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule,
     FormsModule
   ],
